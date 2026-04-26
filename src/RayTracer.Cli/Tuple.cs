70d25682ba8f5e997fc8f782d4d9759e8533b069
Max Barker <maxbarker26@gmail.com>
2026-04-25 19:08:03 -0600

    Create tuple class and tuple test file.

---
+++ src/RayTracer.Cli/Tuple.cs
@@ -0,0 +1,35 @@
+﻿namespace RayTracer.Cli;
+
+public class Tuple
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+    public double W { get; set; }
+
+    public Tuple(double x, double y, double z, double w)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+        W = w;
+    }
+
+    public bool IsPoint()
+    {
+        if (W == 1.0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsVector()
+    {
+        if (W == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ tests/RayTracer.Tests/TupleTests.cs
@@ -0,0 +1,28 @@
+﻿namespace RayTracer.Tests;
+
+using RayTracer.Cli;
+
+public class TupleTests
+{
+    ///<summary>
+    /// A tuple with a W value of 1.0 is a point.
+    ///</summary>
+    [Fact]
+    public void IsPoint_DeterminesIfTupleIsAPoint_True()
+    {
+        Tuple point = new Tuple(5.2, -6.3, 1.7, 1.0);
+        Assert.Equal(point.W, 1.0);
+        Assert.True(point.IsPoint());
+    }
+
+    ///<summary>
+    /// A tuple with a W value of 0 is a vector.
+    ///</summary>
+    [Fact]
+    public void IsVector_DeterminesIfTupleIsAVector_True()
+    {
+        Tuple vector = new Tuple(5.2, -6.3, 1.7, 0);
+        Assert.Equal(vector.W, 0);
+        Assert.True(vector.IsVector());
+    }
+}
