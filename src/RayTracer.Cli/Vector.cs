d74ba7bf3ef7113773383b916e11db1e56952cdf
Max Barker <maxbarker26@gmail.com>
2026-04-26 21:53:25 -0600

    Create Normalized property which holds a reference to a normalized version of a Vector.

--- src/RayTracer.Cli/Vector.cs
+++ src/RayTracer.Cli/Vector.cs
@@ -2,11 +2,26 @@
 
 public class Vector : Tuple
 {
+    private Vector? _normalized;
     public double Magnitude { get; }
+    public Vector Normalized
+    {
+        get
+        {
+            if (_normalized is null)
+                CalculateNormalized();
+            return _normalized;
+        }
+    }
 
     public Vector(double x, double y, double z)
         : base(x, y, z, 0)
     {
         Magnitude = Math.Sqrt((x * x) + (y * y) + (z * z));
+    }
+
+    private void CalculateNormalized()
+    {
+        _normalized = new(X / Magnitude, Y / Magnitude, Z / Magnitude);
     }
 }
--- tests/RayTracer.Tests/VectorTests.cs
+++ tests/RayTracer.Tests/VectorTests.cs
@@ -4,11 +4,26 @@ using RayTracer.Cli;
 
 public class VectorTests()
 {
+    ///<summary>
+    /// Tests the magnitude property which is unique to the Vector class.
+    ///</summary>
     [Fact]
-    public void Magnitude_ReturnTheMagnitudeOfAVectorObject_MagnatudeEqualsExpected()
+    public void Magnitude_TheMagnitudeOfAVectorObject_MagnatudeEqualsExpected()
     {
         Vector vector = new(2, 3, 4);
         double expectedMagnitude = Math.Sqrt(29);
         Assert.Equal(expectedMagnitude, vector.Magnitude, 0.000000001);
+    }
+
+    ///<summary>
+    /// Tests the Normalized property which is unique to the Vector class.
+    ///</summary>
+    [Fact]
+    public void Normalized_ANormalizedVersionOfAGivenVector_NormalizedVectorEqualsExpected()
+    {
+        Vector vector = new(2, 3, 4);
+        double magnitude = Math.Sqrt(29);
+        Vector expectedNormalized = new(2 / magnitude, 3 / magnitude, 4 / magnitude);
+        Assert.Equal(expectedNormalized, vector.Normalized);
     }
 }
