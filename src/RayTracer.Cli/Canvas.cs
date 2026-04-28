aa61d052b50e8b5478b5c1b0e59ba67584c4db53
Max Barker <maxbarker26@gmail.com>
2026-04-27 21:37:15 -0600

    Initialize every pixel to black add getter and setter methods.

--- src/RayTracer.Cli/Canvas.cs
+++ src/RayTracer.Cli/Canvas.cs
@@ -2,6 +2,7 @@
 
 public class Canvas
 {
+    public Color[,] _pixelMatrix;
     public int Width { get; }
     public int Height { get; }
 
@@ -9,5 +10,29 @@ public class Canvas
     {
         Width = width;
         Height = height;
+        _pixelMatrix = new Color[width, height];
+        // Initialize all pixels to black
+        SetAllPixels(new Color(0, 0, 0));
+    }
+
+    public void SetAllPixels(Color color)
+    {
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                _pixelMatrix[i, j] = color;
+            }
+        }
+    }
+
+    public void SetPixel(int x, int y, Color color)
+    {
+        _pixelMatrix[x, y] = color;
+    }
+
+    public Color GetPixel(int x, int y)
+    {
+        return _pixelMatrix[x, y];
     }
 }
--- tests/RayTracer.Tests/CanvasTests.cs
+++ tests/RayTracer.Tests/CanvasTests.cs
@@ -5,10 +5,23 @@ using RayTracer.Cli;
 public class CanvasTests()
 {
     [Fact]
-    public void WidthAndHeight_WidthAndHeightAreSetWithConstructor_AreAsExpected()
+    public void CanvasConstructor_WidthAndHeightAreSetWithConstructor_AreAsExpected()
     {
         Canvas canvas = new Canvas(10, 20);
         Assert.Equal(10, canvas.Width);
         Assert.Equal(20, canvas.Height);
+    }
+
+    [Fact]
+    public void CanvasConstructor_PixelColorsAreInitializedInConstructor_AllPixelsAreBlack()
+    {
+        Canvas canvas = new Canvas(10, 20);
+        for (int i = 0; i < canvas.Width; i++)
+        {
+            for (int j = 0; j < canvas.Width; j++)
+            {
+                Assert.Equal(new Color(0, 0, 0), canvas.GetPixel(i, j));
+            }
+        }
     }
 }
