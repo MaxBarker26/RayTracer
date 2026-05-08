d8c20c14af29ea4b6fc36dc066b39b4587995511
Max Barker <maxbarker26@gmail.com>
2026-05-07 18:49:30 -0600

    Add tests for rays intersecting spheres.

--- tests/RayTracer.Tests/RayTests.cs
+++ tests/RayTracer.Tests/RayTests.cs
@@ -22,4 +22,56 @@ public class RayTests
         Assert.Equal(new Point(1, 3, 4), r.Position(-1));
         Assert.Equal(new Point(4.5, 3, 4), r.Position(2.5));
     }
+
+    [Fact]
+    public void Intersects_RayIntersectingSphere_RayPassesThroughSphereAtExpectedPoints()
+    {
+        Sphere s = new();
+        Ray r = new(new Point(0, 0, -5), new Vector(0, 0, 1));
+        double[] xs = r.Intersects(s);
+        Assert.Equal(xs.Length, 2);
+        Assert.Equal(4.0, xs[0]);
+        Assert.Equal(6.0, xs[1]);
+    }
+
+    [Fact]
+    public void Intersects_RayTangentToASphere_RayIntersectsAtOnePointOnTheSphere()
+    {
+        Sphere s = new();
+        Ray r = new(new Point(0, 1, -5), new Vector(0, 0, 1));
+        double[] xs = r.Intersects(s);
+        Assert.Equal(5.0, xs[0]);
+        Assert.Equal(5.0, xs[1]);
+    }
+
+    [Fact]
+    public void Intersects_RayMissesSphere_RayIntersectsAtNoPointOnTheSphere()
+    {
+        Sphere s = new();
+        Ray r = new(new Point(0, 2, -5), new Vector(0, 0, 1));
+        double[] xs = r.Intersects(s);
+        Assert.Equal(xs.Length, 0);
+    }
+
+    [Fact]
+    public void Intersects_RayOriginatesInsideSphere_RayPassesThroughSphereAtExpectedPoints()
+    {
+        Sphere s = new();
+        Ray r = new(new Point(0, 0, 0), new Vector(0, 0, 1));
+        double[] xs = r.Intersects(s);
+        Assert.Equal(xs.Length, 2);
+        Assert.Equal(-1.0, xs[0]);
+        Assert.Equal(1.0, xs[1]);
+    }
+
+    [Fact]
+    public void Intersects_RayOriginatesBehindSphere_RayPassesThroughSphereAtExpectedPoints()
+    {
+        Sphere s = new();
+        Ray r = new(new Point(0, 0, 0), new Vector(0, 0, 1));
+        double[] xs = r.Intersects(s);
+        Assert.Equal(xs.Length, 2);
+        Assert.Equal(-6.0, xs[0]);
+        Assert.Equal(-4.0, xs[1]);
+    }
 }
