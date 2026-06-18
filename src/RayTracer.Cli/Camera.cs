bb2aa596f7dc66078fdebe9935e2a42cb69d3c89
Max Barker <maxbarker26@gmail.com>
2026-06-17 23:43:47 -0600

    Create method to determine pixel size in world space for camera.

--- src/RayTracer.Cli/Camera.cs
+++ src/RayTracer.Cli/Camera.cs
@@ -2,14 +2,37 @@
 
 public class Camera
 {
-  public int hSize {get;}
-  public int vSize {get;}
-  public double fieldOfView {get; set;}
-  public Matrix transform {get; set;} = Matrix.Identity();
+    public double HSize { get; }
+    public double VSize { get; }
+    public double FieldOfView { get; }
+    public Matrix Transform { get; set; } = Matrix.Identity();
 
-  public Camera(int hSize, int vSize, double fieldOfView) {
-    this.hSize = hSize;
-    this.vSize = vSize;
-    this.fieldOfView = fieldOfView;
-  }
+    public double PixelSize { get; private set; }
+
+    public Camera(int hSize, int vSize, double fieldOfView)
+    {
+        this.HSize = hSize;
+        this.VSize = vSize;
+        this.FieldOfView = fieldOfView;
+        SetPixelSize();
+    }
+
+    private void SetPixelSize()
+    {
+        double halfView = Math.Tan(FieldOfView / 2);
+        double aspect = HSize / VSize;
+        double halfWidth;
+        double halfHeight;
+        if (aspect >= 1)
+        {
+            halfWidth = halfView;
+            halfHeight = halfView / aspect;
+        }
+        else
+        {
+            halfHeight = halfView;
+            halfWidth = halfView * aspect;
+        }
+        PixelSize = (halfWidth * 2) / HSize;
+    }
 }
--- tests/RayTracer.Tests/CameraTests.cs
+++ tests/RayTracer.Tests/CameraTests.cs
@@ -1,16 +1,33 @@
 namespace RayTracer.Tests;
+
 using RayTracer.Cli;
 
-public class CameraTests() {
-  [Fact]
-  public void CameraConstructor() {
-    Camera c = new(160, 120, Math.PI/2);
-    
-    Assert.Equal(160, c.hSize);
-    Assert.Equal(120, c.vSize);
-    Assert.Equal(Math.PI/2, c.fieldOfView);
-    Assert.Equal(Matrix.Identity(), c.transform);
-  }
+public class CameraTests()
+{
+    [Fact]
+    public void CameraConstructor()
+    {
+        Camera c = new(160, 120, Math.PI / 2);
+
+        Assert.Equal(160, c.HSize);
+        Assert.Equal(120, c.VSize);
+        Assert.Equal(Math.PI / 2, c.FieldOfView);
+        Assert.Equal(Matrix.Identity(), c.Transform);
+    }
+
+    [Fact]
+    public void PixelSize_PixelSizeForHorizontalCanvas_EqualsExpected()
+    {
+        Camera c = new(200, 125, Math.PI / 2);
+
+        Assert.Equal(0.01, c.PixelSize, 0.0001);
+    }
+
+    [Fact]
+    public void PixelSize_PixelSizeForVerticalCanvas_EqualsExpected()
+    {
+        Camera c = new(125, 200, Math.PI / 2);
+
+        Assert.Equal(0.01, c.PixelSize, 0.0001);
+    }
 }
-
-
